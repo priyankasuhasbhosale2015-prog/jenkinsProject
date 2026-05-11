3939da882cb53d064a6d7fad86c6a7dd7257d284
Priyanka Bhosale <priyankasuhasbhosale.2015@gmail.com>
2026-05-11 21:33:33 +0530

    initial commit

---
+++ src/main/java/mar2026/jenkins_practice/App.java
@@ -0,0 +1,13 @@
+package mar2026.jenkins_practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mar2026/jenkins_practice/LoginTest.java
@@ -0,0 +1,12 @@
+package mar2026.jenkins_practice;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+   @Test
+   void hello()
+   {
+	System.out.println("LOGIN TEST IS WORKING****");
+   }
+
+ }
